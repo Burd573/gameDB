843d10565537a4464667b1545e252a0796d541f3
burd573 <burd573@gmail.com>
2021-02-22 21:25:29 -0700

    Implemented connection to DB. Temporarily prints game names after connecting

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -1,51 +1,135 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class GUI extends Application
 {
-
+    Map<String, String> dbInput = new HashMap<>();
 
     @Override
     public void start(Stage primaryStage)
     {
         primaryStage.setTitle("Game Database");
+        enterDBInfo();
+//        for(Map.Entry<String,String> map: dbInput.entrySet())
+//        {
+//            System.out.println(map.getKey() + " -> " + map.getValue());
+//        }
 
-        Label dbURL = new Label("Enter the url to the database: ");
-        Label uName = new Label("Enter the username: ");
-        Label pwd = new Label("Enter password: ");
-        Label driverURL = new Label("Enter the url for the driver: ");
+        primaryStage.show();
+    }
+
+    public void enterDBInfo()
+    {
+        Stage stage = new Stage();
+        VBox vbox = new VBox();
+        GridPane gridpane = new GridPane();
+
+        Label dbUrlLabel = new Label("Enter the url to the database: ");
+        Label uNameLabel = new Label("Enter the username: ");
+        Label pwdLabel = new Label("Enter password: ");
+        Label driverUrlLabel = new Label("Enter the url for the driver: ");
 
         TextField dbURLField = new TextField();
         TextField uNameField = new TextField();
         TextField pwdField = new PasswordField();
-        TextField driverURLField = new TextField();
+        TextField driverURLField = new TextField();;
+
+        gridpane.add(dbUrlLabel,0,0);
+        gridpane.add(uNameLabel,0,1);
+        gridpane.add(pwdLabel,0,2);
+        gridpane.add(driverUrlLabel,0,3);
+
+        gridpane.add(dbURLField,1,0);
+        gridpane.add(uNameField,1,1);
+        gridpane.add(pwdField,1,2);
+        gridpane.add(driverURLField,1,3);
 
         Button okButton = new Button("OK");
+        okButton.setPrefWidth(50);
 
-        GridPane root = new GridPane();
-        root.add(dbURL,0,0);
-        root.add(uName,0,1);
-        root.add(pwd,0,2);
-        root.add(driverURL,0,3);
+        vbox.getChildren().addAll(gridpane,okButton);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setMinSize(250,150);
+        vbox.setMargin(gridpane,new Insets(10,10,10,10));
 
-        root.add(dbURLField,1,0);
-        root.add(uNameField,1,1);
-        root.add(pwdField,1,2);
-        root.add(driverURLField,1,3);
+        Scene scene = new Scene(vbox);
+        stage.setScene(scene);
+        stage.show();
 
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        okButton.setOnAction(e -> {
+            String url = "DbUrl";
+            String uName = "username";
+            String pwd = "password";
+            String driverUrl = "driverUrl";
 
+            dbInput.put(url,dbURLField.getText());
+            dbInput.put(uName,uNameField.getText());
+            dbInput.put(pwd,pwdField.getText());
+            dbInput.put(driverUrl,driverURLField.getText());
+            connectToDB();
 
+            stage.hide();
+        });
     }
+
+    public void connectToDB()
+    {
+        Connection conn = null;
+        ResultSet rs = null;
+        Statement stmt = null;
+
+        String _url = dbInput.get("DbUrl");
+        String userName = dbInput.get("username");
+        String pwd = dbInput.get("password");
+
+        //Execute relevant method based off argument entered in command line
+        try {
+            Class.forName(dbInput.get("driverUrl"));
+            conn = DriverManager.getConnection(_url, userName, pwd);
+
+            stmt = conn.createStatement();
+
+            //SQL query
+            rs = stmt.executeQuery("SELECT name FROM game");
+            while(rs.next())
+            {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        } finally
+        {
+            //close the connection
+            try
+            {
+                if (conn != null)
+                {
+                    conn.close();
+                }
+            } catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
