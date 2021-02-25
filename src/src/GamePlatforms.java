41357f29a8651b51933ac4b4a00a68545ee2e7e2
Burd573 <burd573@gmail.com>
2021-02-25 13:33:05 -0700

    Added the rest of the buttons and game info

---
+++ src/src/GamePlatforms.java
@@ -0,0 +1,21 @@
+package src;
+
+public class GamePlatforms
+{
+    String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public GamePlatforms(String name)
+    {
+        this.name = name;
+    }
+}
---
+++ src/src/GameReviews.java
@@ -0,0 +1,45 @@
+package src;
+
+public class GameReviews
+{
+    private String reviewerName;
+    private Double score;
+    private String comment;
+
+    public GameReviews(String reviewerName, Double score, String comment)
+    {
+        this.reviewerName = reviewerName;
+        this.score = score;
+        this.comment = comment;
+    }
+
+    public String getReviewerName()
+    {
+        return reviewerName;
+    }
+
+    public void setReviewerName(String reviewerName)
+    {
+        this.reviewerName = reviewerName;
+    }
+
+    public Double getScore()
+    {
+        return score;
+    }
+
+    public void setScore(Double score)
+    {
+        this.score = score;
+    }
+
+    public String getComment()
+    {
+        return comment;
+    }
+
+    public void setComment(String comment)
+    {
+        this.comment = comment;
+    }
+}
